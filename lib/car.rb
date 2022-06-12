1dd3badae4d2e22747628b6d54687cf62619e63d
Akilah Snowden <akilahdsnowden@gmail.com>
2022-06-12 19:16:40 -0400

    commit tree d7cb26b0c0a08320e593604836050be9e217539a

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,9 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+# < is used to inherit the class from vehicle(parent class)
+class Car < Vehicle
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,16 @@
 class Vehicle
+    attr_accessor :wheel size, :wheel_number
 
+    def intialize(wheel_size, wheel_number)
+        @wheel = wheel_size
+        @wheel_number = wheel_number
+    end
+
+    def go
+        "vrrrrrrrooom!"
+    end
+
+    def fill_up_tank
+        "filling up!"
+    end
 end
